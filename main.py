bd46d8f0084bf54bf2dcda73af727330066e977b
Duncan Miller <dmiller@heatspring.com>
2024-01-04 15:59:53 -0800

    perform tensor operations

--- main.py
+++ main.py
@@ -5,3 +5,10 @@ from tinygrad.tensor import Tensor
 t1 = Tensor([1, 2, 3, 4, 5])
 
 print(t1.numpy())
+
+full = Tensor.full(shape=(2, 3), fill_value=5)
+
+t4 = Tensor([1, 2, 3, 4, 5])
+t5 = (t4 + 1) * 2
+t6 = (t5 * t4).relu().log_softmax()
+print(t6.numpy())
